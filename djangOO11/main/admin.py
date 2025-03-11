8439e2da6dbe359a70a13aa577e5832686075425
Bekzat022 <b57776447@gmail.com>
2025-03-11 14:52:47 +0600

    first commit

---
+++ djangOO11/main/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+# Register your models here.
+from main.models import Main, AboutUs, Team, Blog, ContactUs
+
+@admin.register(Main)
+class MainAdmin(admin.ModelAdmin):
+    list_display = ('site_name',)
+
+
+
+@admin.register(AboutUs)
+class AboutUsAdmin(admin.ModelAdmin):
+    list_display = ("description",'photo','year_in_business', 'happy_people', 'sales', 'award_winning')
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("name", "photo", "job_title","instagram", 'facebook')
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = ('address','phone_number1','phone_number2','email')
